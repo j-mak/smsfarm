f13f710067b75a7247ec12a9afc77367abeffa14
Jozef Mak <sunny@jozefmak.eu>
2018-02-27 20:45:46 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,29 @@
 from setuptools import setup
 
+
+def readme():
+    with open('README.md') as file:
+        return file.read()
+
+
+def license_file():
+    with open('LICENSE.md') as file:
+        return file.read()
+
+
 setup(
     name='smsfarm',
-    version='0.0.1',
+    version='0.1.0',
     packages=['smsfarm'],
     url='https://github.com/j-mak/smsfarm',
-    license='MIT',
+    license=license_file(),
     author='Jozef "sunny" Mak',
     author_email='sunny@jozefmak.eu',
     description='Python client for smsfarm.sk',
-    long_description='',
+    long_description=readme(),
     keywords=[
-        'smsfarm'
+        'smsfarm',
+        'sms-client'
     ],
     classifiers=[
 
